4000854295af6c791770b17dc2b4da22a5b1f10a
glacerous <azzakyraihan@gmail.com>
2025-11-10 23:53:32 +0700

    Fix Prisma Accelerate configuration

--- lib/db.ts
+++ lib/db.ts
@@ -1,19 +1,16 @@
-// 1. Ganti import dari '@prisma/client' menjadi '@prisma/client/edge'
-import { PrismaClient } from '@prisma/client/edge'
-
-// 2. Import ekstensi Accelerate
+import { PrismaClient } from '@prisma/client'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined
+const globalForPrisma = globalThis as unknown as {
+  prisma: ReturnType<typeof createPrismaClient> | undefined
 }
 
-// 3. Tambahkan ekstensi Accelerate saat membuat instance baru.
-// Kita tidak perlu menambahkannya jika instance sudah ada (global.prisma).
-// Pastikan Anda memanggil .extends() setelah new PrismaClient().
-export const prisma = global.prisma || new PrismaClient().$extends(withAccelerate())
+function createPrismaClient() {
+  return new PrismaClient().$extends(withAccelerate())
+}
+
+export const prisma = globalForPrisma.prisma ?? createPrismaClient()
 
 if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma
+  globalForPrisma.prisma = prisma
 }
